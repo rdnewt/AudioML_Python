25e3d8146b712aebffbb7f3de24b27fa8451c2b9
Rachel <rrhoade2@asu.edu>
2019-11-07 15:51:55 -0700

    Forgot to save initial_test before last commit

--- initial_test.py
+++ initial_test.py
@@ -39,6 +39,25 @@ def create_spectrogram_test(filename,name):
     plt.close(fig)
     plt.close('all')
     del filename,name,clip,sample_rate,fig,ax,S
+
+def create_spectrogram_train(filename,name):
+    plt.interactive(False)
+    clip, sample_rate = librosa.load(filename, sr=None)
+    fig = plt.figure(figsize=[0.72,0.72])
+    ax = fig.add_subplot(111)
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.set_frame_on(False)
+    S = librosa.feature.melspectrogram(y=clip, sr=sample_rate)
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
+    filename  = r"C:\Users\rrhoa\Documents\Classes\EEE488\ESC_capstone\test_files\\" + name + ".png"
+    fig.savefig(filename, dpi=400, bbox_inches='tight',pad_inches=0)
+    plt.close()    
+    fig.clf()
+    plt.close(fig)
+    plt.close('all')
+    del filename,name,clip,sample_rate,fig,ax,S
+
     
 filenames = os.listdir(directory)
 test_names = []
