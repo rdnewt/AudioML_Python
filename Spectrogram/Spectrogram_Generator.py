f838827fab65f89b7dee1052180b908c9c8e36ea
sjharva <57581357+sjharva@users.noreply.github.com>
2020-02-25 01:39:47 -0700

    Add files via upload
    To be used for converting Urban8k data set into spectrograms for CNN training.

---
+++ Spectrogram/Spectrogram_Generator.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 22:15:10 2020
+
+@author: sjhar
+"""
+
+### Spectrogram Module- this will get consolidated into one (or a couple) of functions which will be added to the audioml module
+
+
+import os
+import numpy as np
+import scipy.io.wavfile as spwav
+import scipy.signal as spsig
+import matplotlib.pyplot as plt
+
+
+## This section is to be modified to grab all the sound clip names in your local file directory
+## Rachel, I know you already have a segment of code that does this in your python file uploaded to github
+## just need to get all the full path+file strings into the list 'fnames'
+directory = r"C:\Users\sjhar\OneDrive\Documents\Classes\ASU\2020 Spring\EEE 489 Senior Design II\Code\Audio Samples"
+fnames = os.listdir(directory)
+#fnames[:] = [directory+"\\"+name for name in fnames]
+
+# where the spectrograms will be stored
+output_path = r"C:\Users\sjhar\OneDrive\Documents\Classes\ASU\2020 Spring\EEE 489 Senior Design II\Code\Spectrograms"
+
+
+## I played around with these parameters to find a figure size around 400-500 points square, these seemed to work
+## Selected 2048 point fft to maintain resolution while "cutting out" the highest half of frequencies in the fft
+## Spectrograms were pretty much empty above ~10kHz, so I decided to chop out that data below and maintain 512 points in frequency
+pts_fft = 2048  # pts in each fft of the spectrogram, scipy only returns half of these, and then I half that again due to ^^^
+step = 512      # chosen to maintain ~400 points on the time axis, was arbitrary but I decided to keep roughly the same number of pts along both axes
+pts_overlap = pts_fft-step
+spectro_pts_5s = (44100*5)//(step)      # calculates the number of x-axis points in the spectrogram
+
+# the scipy spectrogram chooses to 'cut off' any data points that fall beyond the last complete fft
+# this basically calculates the length of array needed so that no data is cut off for a 5 second clip
+dat_pts_5s = spectro_pts_5s*step+pts_overlap
+
+
+
+for name in fnames:
+    fs,dat = spwav.read(directory+"\\"+name)           #read in .wav file
+    
+    # this section performs a check to see if the .wav file is stereo (2-channels) 
+    # and averages the 2 to a single channel if that is the case
+    # can be commented out if all the files are now mono format
+    if len(np.shape(dat))==2:
+        dat = dat.astype(float)
+        dat = (dat[:,0]+dat[:,1])//2
+        dat = dat.astype('int16')
+    
+    # this 'if-else' sets all arrays to the correct length to return a 5-second length spectrogram
+    if len(dat) > dat_pts_5s:
+        dat = dat[0:dat_pts_5s]
+    else:
+        dat = np.concatenate((dat,np.zeros(dat_pts_5s-len(dat),'int16')))
+    
+    # generates spectrogram
+    f, t, Sxx = spsig.spectrogram(dat, fs, window='hamming', nperseg=pts_fft, noverlap=pts_overlap)
+    
+    # clips frequency labels and output spectrogram to half-length
+    # fft will calculate for frequencies 0Hz-22050Hz, this clips that to 0Hz-11025Hz
+    # again, very little data was present above ~10kHz, I think this will improve our resolution and remove empty data points
+    f = f[0:pts_fft//4]
+    Sxx = Sxx[0:pts_fft//4,:]
+    
+    
+    # plot output in standard colormesh chart, should be commented out when running for large batch of data
+    plt.pcolormesh(t, f, Sxx)
+    plt.ylabel('Freq (Hz)')
+    plt.xlabel('Time (s)')
+    plt.show()
+    
+    
+    #print(np.shape(Sxx))                   # shows shape of array holding spectro data
+    spectro = np.array((f,t,Sxx))           # load spectrogram elements into a single numpy array
+    outname = output_path+"\\"+name[0:-4]+"_spectro"        # specify output file full name (path + filename)
+    np.save(outname,spectro)          # save array
+    
+
+
+
+    
+    
