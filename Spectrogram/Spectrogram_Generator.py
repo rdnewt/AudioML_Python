2f7696b0a087919bb66e2f3fce4a6b9d9dfcd6a6
sjharva <57581357+sjharva@users.noreply.github.com>
2020-02-25 23:43:22 -0700

    Add files via upload
    Updated to include transpose of spectrogram data and increase in fft resolution to 4096 points with frequency limit set to ~5kHz (result in 512 data points in frequency axis)

--- Spectrogram/Spectrogram_Generator.py
+++ Spectrogram/Spectrogram_Generator.py
@@ -6,7 +6,9 @@ Created on Sun Feb 23 22:15:10 2020
 """
 
 ### Spectrogram Module- this will get consolidated into one (or a couple) of functions which will be added to the audioml module
-
+### Output spectrogram dimesnions:
+###     430 points along time-axis (x-axis)
+###     512 points along frequency-axis (y-axis)
 
 import os
 import numpy as np
@@ -27,16 +29,16 @@ output_path = r"C:\Users\sjhar\OneDrive\Documents\Classes\ASU\2020 Spring\EEE 48
 
 
 ## I played around with these parameters to find a figure size around 400-500 points square, these seemed to work
-## Selected 2048 point fft to maintain resolution while "cutting out" the highest half of frequencies in the fft
-## Spectrograms were pretty much empty above ~10kHz, so I decided to chop out that data below and maintain 512 points in frequency
-pts_fft = 2048  # pts in each fft of the spectrogram, scipy only returns half of these, and then I half that again due to ^^^
+## Selected 4096 point fft to maintain resolution while "cutting out" the highest half of frequencies in the fft
+## Spectrograms were pretty much empty above ~5kHz, so I decided to chop out that data below and maintain 512 points in frequency
+pts_fft = 4096  # pts in each fft of the spectrogram, scipy only returns half of these, and then I half that again due to ^^^
 step = 512      # chosen to maintain ~400 points on the time axis, was arbitrary but I decided to keep roughly the same number of pts along both axes
 pts_overlap = pts_fft-step
 spectro_pts_5s = (44100*5)//(step)      # calculates the number of x-axis points in the spectrogram
 
 # the scipy spectrogram chooses to 'cut off' any data points that fall beyond the last complete fft
 # this basically calculates the length of array needed so that no data is cut off for a 5 second clip
-dat_pts_5s = spectro_pts_5s*step+pts_overlap
+dat_pts_5s = spectro_pts_5s*step+pts_fft-1
 
 
 
@@ -61,23 +63,25 @@ for name in fnames:
     f, t, Sxx = spsig.spectrogram(dat, fs, window='hamming', nperseg=pts_fft, noverlap=pts_overlap)
     
     # clips frequency labels and output spectrogram to half-length
-    # fft will calculate for frequencies 0Hz-22050Hz, this clips that to 0Hz-11025Hz
-    # again, very little data was present above ~10kHz, I think this will improve our resolution and remove empty data points
-    f = f[0:pts_fft//4]
-    Sxx = Sxx[0:pts_fft//4,:]
+    # fft will calculate for frequencies 0Hz-22050Hz, this clips that to 0Hz-~5500Hz
+    # again, very little data was present above ~5kHz, I think this will improve our resolution and remove empty data points
+    f = f[0:pts_fft//8]
+    Sxx = Sxx[0:pts_fft//8,:]
+    #print(np.shape(Sxx))
     
-    
-    # plot output in standard colormesh chart, should be commented out when running for large batch of data
+    '''# plot output in standard colormesh chart, should be commented out when running for large batch of data
     plt.pcolormesh(t, f, Sxx)
     plt.ylabel('Freq (Hz)')
     plt.xlabel('Time (s)')
     plt.show()
-    
+    '''
     
     #print(np.shape(Sxx))                   # shows shape of array holding spectro data
-    spectro = np.array((f,t,Sxx))           # load spectrogram elements into a single numpy array
+    # load spectrogram elements into a single numpy array and transpose Sxx
+    # transpose Sxx to allow easier concatenation of new data chunks in main program, CNN needs to be trained with the correctly formatted data
+    spectro = np.array((f,t,np.transpose(Sxx)))
     outname = output_path+"\\"+name[0:-4]+"_spectro"        # specify output file full name (path + filename)
-    np.save(outname,spectro)          # save array
+    #np.save(outname,spectro)          # save array
     
 
 
