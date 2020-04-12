18983bbfeee140ac1977c555c815cd3dd4eeb0d3
sjharva <57581357+sjharva@users.noreply.github.com>
2020-04-11 21:55:25 -0700

    Update audioml.py
    Updated this to be the module containing the processing functions that we're using. Subsequent updates will include GPU-optimized versions of the function, and a 'check' on the python environment name for switching to using those functions when our program is run on the jetson nano.

--- audioml.py
+++ audioml.py
@@ -1,53 +1,48 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 28 11:34:35 2020
+Created on Mon Feb  3 00:13:49 2020
 
-@author: solasholola
+@author: sjhar
 """
 
-import pyaudio
-import wave
+import numpy as np
+import scipy.io.wavfile as wav
+### our custom module with all of our function
+import audioml as aml
 
-FORMAT = pyaudio.paInt16  # Records in 16bits
 
-CHANNELS = 1  # Mono channel
+pts_fft = 1024
+window = np.hamming(pts_fft)
 
-RATE = 44100 # Sample Rate
+def dataload (filename):
+    fs,dat = wav.read(filename)           #read in .wav file
+#*    #print(filename, fs)
+    # this section performs a check to see if the .wav file is stereo (2-channels) 
+    # and averages the 2 to a single channel if that is the case
+    # can be commented out if all the files are now mono format
+    if len(np.shape(dat))==2:
+        dat = dat.astype(float)
+        dat = (dat[:,0]+dat[:,1])//2
+        dat = dat.astype('int16')
+    return dat, fs
 
-CHUNK = 1024  #byte size
-
-RECORD_SECONDS = 5
-
-WAVE_OUTPUT_FILENAME = "file.wav"
-
-audio = pyaudio.PyAudio()
-
-# start Recording
-stream = audio.open(format = FORMAT, 
-                    channels = CHANNELS,
-                    rate = RATE, 
-                    input = True,
-                    frames_per_buffer = CHUNK,
-                    # USB 
-                    input_device_index = 0)
-                    
-print ("recording...")
-frames = []
-for i in range(0, int((RATE / CHUNK) * RECORD_SECONDS)):
-    data = stream.read(CHUNK, exception_on_overflow = False)
-    frames.append(data)
-    print ("finished recording")
-
-# stop Recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
-
-# creates wave file with audio read in
-waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
+def spec_gen(dat, fs):
+    dat = np.pad(dat, (0,pts_fft-len(dat)%pts_fft))
+    dat_l = len(dat)
+    if fs != 44100:
+            print ("Error:\n    Loc: audioml.py, class: sample, function: spec_gen\n    Desc: sampling frequency does not equal 44.1kHz\n        Support for other frequencies will be added in version 2.0.")
+            return False
+    else:
+        spec_l = len(dat)//pts_fft
+        spec = np.zeros((pts_fft//4,spec_l))
+        for i in range(spec_l):
+            a = np.fft.fft(dat[i:i+pts_fft]*window)
+            b = abs(a[0:pts_fft//4]+1E-15)  #added 1E-10 to values because np.log10(b) was producing a divide by zero warning, 1E-10 far too small to affect output
+            b = 20*np.log10(b)
+            spec[:,i] = b
+            del a, b
+        
+        f = np.arange(0,fs/4,fs/pts_fft)
+        t = np.arange(0,dat_l/fs,pts_fft/fs)
+        
+        return spec, f, t
